ff3e978b5b481a78e43b3f9cbb06e6a32bdc6826
Misa Liu <misaliu@misaliu.top>
2025-09-26 18:46:55 +0800

    feat(loader): Automatically build d.ts file

--- packages/loader/vite.config.ts
+++ packages/loader/vite.config.ts
@@ -1,11 +1,19 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite'
+import dts from 'unplugin-dts/vite';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: './src-lib/main.ts',
+      entry: resolve(__dirname, 'src-lib/main.ts'),
       name: 'Counter',
       fileName: 'counter',
     },
   },
+  plugins: [
+    dts({
+      tsconfigPath: './tsconfig.json',
+      bundleTypes: true
+    }),
+  ],
 })
