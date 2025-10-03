1ecda613ad0faa42bdbaa3f069a49b95060b8d59
Misa Liu <misaliu@misaliu.top>
2025-10-03 15:24:53 +0800

    feat(manager): Show game name

--- packages/manager/src/PlatformInfo.tsx
+++ packages/manager/src/PlatformInfo.tsx
@@ -4,6 +4,7 @@ export const PlatformInfo = () => {
     <>
       <h2>Platform Info</h2>
       <ul id="platform-info">
+        <li>Running on: {window.YASCML.stats.gameName}</li>
         <li>{window.SugarCube!.version.long()}</li>
         <li>YASCML v{window.YASCML.version}</li>
       </ul>
