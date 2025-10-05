4594986c89b58755900b11ca018f8c756bc0df61
Misa Liu <misaliu@misaliu.top>
2025-10-06 01:13:16 +0800

    [skip ci] feat(api): Hook `Image.src`

--- packages/api/src/init.ts
+++ packages/api/src/init.ts
@@ -1,6 +1,18 @@
 import { replace } from '@yascml/utils';
 import { Passages } from './passages';
 
+// Hook `<image>`
+{
+  const $src = Object.getOwnPropertyDescriptor(HTMLImageElement.prototype, 'src')!;
+  Reflect.defineProperty(HTMLImageElement.prototype, 'src', {
+    get() { $src.get!.call(this) },
+    set(source: string) {
+      // yay let's do something here
+      $src.set!.call(this, source);
+    },
+  });
+}
+
 // Hook SugarCube.Story.get()
 replace(window.SugarCube!.Story, 'get', {
   value(name: string) {
