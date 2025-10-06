a3593b3480831d0343920f88b9cd31fb8f91e695
Misa Liu <misaliu@misaliu.top>
2025-10-06 16:53:17 +0800

    feat(api/passages): Add tags argument

--- packages/api/src/passages.ts
+++ packages/api/src/passages.ts
@@ -4,8 +4,8 @@ type PassageContent = string | Promise<string> | ((name: string) => string | Pro
 
 export const Passages = new Map<string, HTMLElement>();
 
-const addReplacedElement = (name: string, content: string) => {
-  Passages.set(name, createPassageDOM({ name, tags: [], text: content }));
+const addReplacedElement = (name: string, content: string, tags: string[]) => {
+  Passages.set(name, createPassageDOM({ name, tags, text: content }));
 
   // If current passage has been replaced, refresh the current passgae.
   const currentPassageDOM = document.querySelector<HTMLDivElement>('#passages > div[data-passage]');
@@ -20,10 +20,10 @@ const addReplacedElement = (name: string, content: string) => {
  * @param {string} name 
  * @param {PassageContent} content 
  */
-const add = async (name: string, content: PassageContent) => {
-  if (typeof content === 'string') return Passages.set(name, createPassageDOM({ name, tags: [], text: content }));
-  else if (typeof content === 'function') return Passages.set(name, createPassageDOM({ name, tags: [], text: await Promise.resolve(content(name)) }));
-  return Passages.set(name, createPassageDOM({ name, tags: [], text: await Promise.resolve(content) }));
+const add = async (name: string, content: PassageContent, tags: string[] = []) => {
+  if (typeof content === 'string') return Passages.set(name, createPassageDOM({ name, tags, text: content }));
+  else if (typeof content === 'function') return Passages.set(name, createPassageDOM({ name, tags, text: await Promise.resolve(content(name)) }));
+  return Passages.set(name, createPassageDOM({ name, tags, text: await Promise.resolve(content) }));
 };
 
 /**
@@ -32,10 +32,10 @@ const add = async (name: string, content: PassageContent) => {
  * @param {string} name 
  * @param {PassageContent} content 
  */
-const replace = async (name: string, content: PassageContent) => {
-  if (typeof content === 'string') return addReplacedElement(name, content);
-  else if (typeof content === 'function') return addReplacedElement(name, await Promise.resolve(content(name)));
-  return addReplacedElement(name, await Promise.resolve(content));
+const replace = async (name: string, content: PassageContent, tags: string[] = []) => {
+  if (typeof content === 'string') return addReplacedElement(name, content, tags);
+  else if (typeof content === 'function') return addReplacedElement(name, await Promise.resolve(content(name)), tags);
+  return addReplacedElement(name, await Promise.resolve(content), tags);
 };
 
 export default {
