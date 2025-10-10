cef604a222d3264e2a2bcefd5f16a05a1da273ff
Misa Liu <misaliu@misaliu.top>
2025-10-10 16:22:22 +0800

    feat(sc2ml-compact): Add node getter

--- packages/sc2ml-compact/src/class/dataManager.ts
+++ packages/sc2ml-compact/src/class/dataManager.ts
@@ -18,4 +18,32 @@ export class SC2DataManager {
   getModLoader() {
     return this.modLoader;
   }
+
+  /**
+   * Get `<tw-storydata>` node from HTML
+   */
+  get rootNode() {
+    return document.querySelector<HTMLElement>('tw-storydata');
+  }
+
+  /**
+   * Get all `<style>` nodes from story root node
+   */
+  get styleNode() {
+    return this.rootNode ? this.rootNode.getElementsByTagName('style') : null;
+  }
+
+  /**
+   * Get all `<script>` nodes from story root node
+   */
+  get scriptNode() {
+    return this.rootNode ? this.rootNode.getElementsByTagName('script') : null;
+  }
+
+  /**
+   * Get all `<tw-passagedata>` nodes from story root node
+   */
+  get passageDataNodeList() {
+    return this.rootNode ? Array.from(this.rootNode.getElementsByTagName('tw-passagedata')) : null;
+  }
 };
