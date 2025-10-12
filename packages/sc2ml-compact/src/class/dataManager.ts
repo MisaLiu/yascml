d08d4666bc91e4617087c98ca7233d1a1300e839
Misa Liu <misaliu@misaliu.top>
2025-10-12 16:30:09 +0800

    [skip ci] feat(sc2ml-compact): Finishing data processing for data manager

--- packages/sc2ml-compact/src/class/dataManager.ts
+++ packages/sc2ml-compact/src/class/dataManager.ts
@@ -1,4 +1,5 @@
 import { ModLoader } from './loader';
+import { SC2DataInfoCache } from './dataInfo';
 import { HtmlTagSrcHook } from './htmlSrcHook';
 import { _Window } from '../types';
 
@@ -6,6 +7,27 @@ export class SC2DataManager {
   readonly thisWin: _Window;
   readonly modLoader: ModLoader;
   readonly htmlTagSrcHook: HtmlTagSrcHook;
+
+  /**
+   * Is this data manager has been initialized?
+   * 
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L247
+   */
+  startInitOk = false;
+  
+  /**
+   * Game data modified by mods.
+   * 
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L302
+   */
+  cSC2DataInfoAfterPatchCache?: SC2DataInfoCache;
+
+  /**
+   * Original game data.
+   * 
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L133
+   */
+  originSC2DataInfoCache?: SC2DataInfoCache;
 
   constructor(window: _Window) {
     this.thisWin = window;
@@ -15,8 +37,178 @@ export class SC2DataManager {
 
   conflictResult = []; // TODO
 
+  /**
+   * Clear {@link originSC2DataInfoCache|SC2DataManager.originSC2DataInfoCache}.
+   * We won't clear {@link cSC2DataInfoAfterPatchCache|SC2DataManager.cSC2DataInfoAfterPatchCache} since
+   * we need to use these data later.
+   * 
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L143
+   */
+  cleanAllCacheAfterModLoadEnd() {
+    if (this.originSC2DataInfoCache) {
+      this.originSC2DataInfoCache.clean();
+      this.originSC2DataInfoCache.destroy();
+      this.originSC2DataInfoCache = (void 0);
+    }
+  }
+
+  /**
+   * Read current game data and return a {@link SC2DataInfoCache} based on it.
+   * 
+   * @returns {SC2DataInfoCache}
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L118
+   */
+  createNewSC2DataInfoFromNow() {
+    return new SC2DataInfoCache(
+      {},
+      'orgin', // https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L121
+      Array.from(this.scriptNode!),
+      Array.from(this.styleNode!),
+      this.passageDataNodeList!,
+    );
+  }
+
+  /**
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L135
+   */
+  earlyResetSC2DataInfoCache() {
+    this.initSC2DataInfoCache();
+    this.flushAfterPatchCache();
+  }
+
+  /**
+   * (Re)create {@link cSC2DataInfoAfterPatchCache|SC2DataManager.cSC2DataInfoAfterPatchCache}.
+   * 
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L326
+   */
+  flushAfterPatchCache() {
+    if (this.cSC2DataInfoAfterPatchCache) {
+      this.cSC2DataInfoAfterPatchCache.destroy();
+      this.cSC2DataInfoAfterPatchCache = (void 0);
+    }
+    this.getSC2DataInfoAfterPatch();
+  }
+
+  /**
+   * Get {@link htmlTagSrcHook|SC2DataManager.htmlTagSrcHook}.
+   * 
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L235
+   */
+  getHtmlTagSrcHook() {
+    return this.htmlTagSrcHook;
+  }
+
+  /**
+   * Get {@link modLoader|SC2DataManager.modLoader}.
+   * 
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L183
+   */
   getModLoader() {
     return this.modLoader;
+  }
+
+  /**
+   * Create {@link cSC2DataInfoAfterPatchCache|SC2DataManager.cSC2DataInfoAfterPatchCache}.
+   * 
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L308
+   */
+  getSC2DataInfoAfterPatch() {
+    this.initSC2DataInfoCache();
+    if (!this.cSC2DataInfoAfterPatchCache) {
+      this.cSC2DataInfoAfterPatchCache = new SC2DataInfoCache(
+        {},
+        'orgin',
+        Array.from(this.scriptNode!),
+        Array.from(this.styleNode!),
+        this.passageDataNodeList!,
+      );
+    }
+    return this.cSC2DataInfoAfterPatchCache;
+  }
+
+  /**
+   * Get (or create) {@link originSC2DataInfoCache|SC2DataManager.originSC2DataInfoCache}.
+   * 
+   * @returns {SC2DataInfoCache}
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L170
+   */
+  getSC2DataInfoCache() {
+    this.initSC2DataInfoCache();
+    return this.originSC2DataInfoCache!;
+  }
+
+  /**
+   * Create {@link originSC2DataInfoCache|SC2DataManager.originSC2DataInfoCache}.
+   * 
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L152
+   */
+  initSC2DataInfoCache() {
+    if (!this.originSC2DataInfoCache) {
+      this.originSC2DataInfoCache = new SC2DataInfoCache(
+        {},
+        'orgin',
+        Array.from(this.scriptNode!),
+        Array.from(this.styleNode!),
+        this.passageDataNodeList!,
+      );
+    }
+  }
+
+  /**
+   * Generate a `<tw-passagedata>` DOM with given passage data.
+   * 
+   * @deprecated We don't need this so it's not implemented.
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L447
+   */
+  makePassageNode() {}
+
+  /**
+   * Generate user script DOM with given script data.
+   * 
+   * @deprecated We don't need this so it's not implemented.
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L480
+   */
+  makeScriptNode() {}
+
+  /**
+   * Generate user style DOM with given style data.
+   * 
+   * @deprecated We don't need this so it's not implemented.
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L467
+   */
+  makeStyleNode() {}
+
+  /**
+   * Patch modified game data to game DOMs.
+   * 
+   * @deprecated We don't need this so it's not implemented.
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L355
+   */
+  patchModToGame() {}
+  
+  /**
+   * Remove exists passage DOMs and add given passage DOMs.
+   * 
+   * @deprecated We don't need this so it's not implemented.
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L525
+   */
+  rePlacePassage() {}
+
+  /**
+   * Initialize this data manager.
+   * 
+   * @see https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader/blob/8a858233f30eaa0617454cf7c14448643c06d2b6/src/BeforeSC2/SC2DataManager.ts#L249
+   */
+  startInit() {
+    if (this.startInitOk) return;
+    this.startInitOk = true;
+
+    this.initSC2DataInfoCache();
+
+    // ... Load mods
+    // ... Check mod dependencies
+    // ... Update mod conflict results
+    // ... Patching modified game data to DOM
   }
 
   /**
@@ -44,6 +236,6 @@ export class SC2DataManager {
    * Get all `<tw-passagedata>` nodes from story root node
    */
   get passageDataNodeList() {
-    return this.rootNode ? Array.from(this.rootNode.getElementsByTagName('tw-passagedata')) : null;
+    return this.rootNode ? Array.from(this.rootNode.getElementsByTagName('tw-passagedata') as HTMLCollectionOf<HTMLElement>) : null;
   }
 };
--- packages/sc2ml-compact/src/main.ts
+++ packages/sc2ml-compact/src/main.ts
@@ -1,5 +1,14 @@
 import lodash from 'lodash';
 import * as idb from 'idb';
+import { SC2DataManager } from './class/dataManager';
 
 if (!window.lodash) window.lodash = lodash;
 if (!window.idb) window.idb = idb;
+
+const dataManager = new SC2DataManager(window);
+
+Object.defineProperty(window, 'modSC2DataManager', {
+  value: dataManager,
+});
+
+window.__AfterInit?.push(dataManager.startInit);
